59162f238628378f7c4638fc01d806d9a848e1a3
Jnanesh Shenoy <148083819+JnaneshShenoy@users.noreply.github.com>
2025-01-18 20:23:59 +0530

    initial commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "<h1>Hehe bwuoii</h1>";
+    }
+}
